4ee57ec4a288f1a866a725e34af1dc49910aedb0
Lawrence Akpan <delaw34@yahoo.com>
2023-05-13 11:40:57 +0100

    Update phonecsv.py
    added more decription

--- phonecsv.py
+++ phonecsv.py
@@ -1,12 +1,13 @@
 #convert a text file to csv file 
+#to enable us access the file we import the os and sys modules
 import os
 import sys
 
-#write a csv file 
 #write a file 
 phone_name = ['samsung \n', 'iphone \n', 'nokia \n', 'hawaii \n']
 phone_name2 = ['motorolla \n', 'redmi\n', 'android \n']
 
+#write a csv file 
 with open(os.path.join(sys.path[0],'phone_name.csv'), 'w') as phone:
     phone_name_write = phone.writelines(phone_name)
     phone_name_write2 = phone.writelines(phone_name2)
